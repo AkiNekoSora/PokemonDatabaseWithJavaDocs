9a0335584c3d9a42f6b2853ed418d7e467a5d9a7
AkiNekoSora <abilby@mail.valenciacollege.edu>
2025-11-08 18:47:21 -0500

    Cutom Combo Box Commit

---
+++ src/main/java/org/pokemondatabase/GUI/CustomGUIDesigns/CustomComboBoxUI.java
@@ -0,0 +1,65 @@
+// Autumn Skye
+// CEN-3024C 13950
+// November 8th, 2025
+package org.pokemondatabase.GUI.CustomGUIDesigns;
+
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboPopup;
+import javax.swing.*;
+import javax.swing.plaf.basic.ComboPopup;
+import java.awt.*;
+
+/**
+ * Used to as the base to call all other custom design choices. (Scroll Lists)
+ * Contains:
+ * <ul>
+ * <li>createArrowButton (calls class)</li>
+ * <li>createPopup</li>
+ * </ul>
+ */
+public class CustomComboBoxUI extends BasicComboBoxUI {
+    public final Color pokemonBlue = new Color(88, 112, 248);
+
+    /**
+     * OVERRIDE
+     * Used to call the customArrowButton class to create the arrow for the GUI. Sending the
+     * specified color.
+     * @return JButton
+     */
+    @Override
+    protected JButton createArrowButton() {
+        JButton button = new CustomArrowButton(pokemonBlue);
+        button.setBackground(Color.WHITE);
+        button.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(true);
+
+        return button;
+    }
+
+    /**
+     * OVERRIDE
+     * Used to create the popup for the drop-down. Calls the CustomScrollBarUI and set the
+     * scroller to the design specified and the background White. Contains createScroller method.
+     * @return ComboPopup
+     */
+    @Override
+    protected ComboPopup createPopup() {
+        BasicComboPopup popup = new BasicComboPopup(comboBox) {
+            /**
+             * OVERRIDE
+             * Calls the CustomScrollBarUI and set the scroller to the design specified and the
+             * background White. Contains createScroller method.
+             * @return JScrollPane
+             */
+            @Override
+            protected JScrollPane createScroller() {
+                JScrollPane scrollPane = super.createScroller();
+                scrollPane.getVerticalScrollBar().setUI(new CustomScrollBarUI());
+                scrollPane.getVerticalScrollBar().setBackground(Color.WHITE);
+                return scrollPane;
+            }
+        };
+        return popup;
+    }
+}
